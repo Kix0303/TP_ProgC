7b2e4d32f5121d1e4fd5470d7087740b258b6cea
Kix0303 <leclainche.killian03@gmail.com>
2025-12-02 10:07:54 +0100

    tri.c

---
+++ TP3/src/tri.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main() {
+    int t[100];
+    int i, j, tmp;
+
+    srand(time(NULL));
+
+    printf("Tableau non trié :\n");
+    for (i = 0; i < 100; i++) {
+        t[i] = rand() % 200 - 100; 
+        printf("%d ", t[i]);
+    }
+    printf("\n");
+
+    for (i = 0; i < 100; i++)
+        for (j = 0; j < 99 - i; j++)
+            if (t[j] > t[j + 1]) {
+                tmp = t[j];
+                t[j] = t[j + 1];
+                t[j + 1] = tmp;
+            }
+
+    printf("Tableau trié :\n");
+    for (i = 0; i < 100; i++)
+        printf("%d ", t[i]);
+
+    return 0;
+}
