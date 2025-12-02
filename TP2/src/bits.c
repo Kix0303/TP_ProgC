884d79ef008f48ade57aa37c953599033a4fcbe0
Kix0303 <leclainche.killian03@gmail.com>
2025-12-02 09:17:17 +0100

    bits.c

--- TP2/src/bits.c
+++ TP2/src/bits.c
@@ -1,1 +1,16 @@
+#include <stdio.h>
 
+int main() {
+    int d = 0x10001000;   // valeur de test (tu peux changer)
+
+    // Bits de gauche 4 et 20 -> bits 28 et 12
+    int bit4gauche  = (d >> 28) & 1;
+    int bit20gauche = (d >> 12) & 1;
+
+    if (bit4gauche == 1 && bit20gauche == 1)
+        printf("1\n");
+    else
+        printf("0\n");
+
+    return 0;
+}
